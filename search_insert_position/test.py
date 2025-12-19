40874c78cff28565e1cd550f5773bc0b93ca77cf
tomihq <hernandeztomas584@gmail.com>
2025-12-19 19:28:23 -0300

    aclaracion de busqueda binaria

--- search_insert_position/test.py
+++ search_insert_position/test.py
@@ -14,3 +14,33 @@ def test_should_found_target_at_right():
 
 def test_should_found_target_going_left_and_right():
     assert(search([1, 2, 3, 6, 7, 8, 9], 5) == 3)
+
+"""
+    Target 5 
+    nums = [1, 2, 3, 6, 7, 8, 9]
+
+    middle = 0 + 6 = 6 / 2 = 3 
+    nums[3] = 6
+
+    5 = 6? no
+    5 < 6? sí
+    high = 3
+
+    --
+    middle = (0 + 3) / 2 = 1
+    nums[1] = 2
+
+    5 = 2? no
+    5 < 2? no
+    low = 1
+    
+    middle = math.floor((1+3) / 2) = 2
+    5 = 3? No.
+    5 < 3? No.
+    low = 2
+
+    --
+    middle = math.floor((2+3) / 2) = 2
+    ¡Acá se me rompió mi código! 
+    El problema es que caigo en 2 siempre. Entonces, a middle siempre le tengo que agregar una unidad porque middle en la próxima ya no es candidato.
+"""
